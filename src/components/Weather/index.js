4039e015f2098640486906f79888aab32730530d
JasonKK <hyeon7427@gmail.com>
2020-07-26 23:47:28 +0900

    Update index.js
    Update index.js

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,16 +1,47 @@
 import React from "react";
-
+const API_CITIES_WEATHERS = "http://localhost:8888/weather-service/weathers?";
 // Stateless component
-const Weather = (props) => {
-  const { cityName } = props.match.params;
-  console.log(cityName);
+class Weather extends React.Component {
+ state = {
+     weatherData: {
+         weather: [
+             {main: "string", description: "string"}
+         ],
+         main: {
+             temp: 0,
+         }
+     },
+ };
 
-  return (
-    <div>
-      <h1>Weather : {cityName}</h1>
-      <p>Here is front page.!</p>
-    </div>
-  );
+    componentDidMount() {
+        const {cityName} =this.props.match.params;
+
+        const currentWeatherData =fetch(API_CITIES_WEATHERS + 'cityName=' + cityName)
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({
+                    weatherData : data,
+                });
+            });
+        //console.log(currentWeatherData);
+    }
+
+    render() {
+        const {cityName} =this.props.match.params;
+        const {weatherData} = this.state;
+        const currentweather = weatherData.weather[0].main
+        const description = weatherData.weather[0].description
+        const temperature = (weatherData.main.temp - 273.15).toFixed(2);
+
+        return (
+            <div>
+                <h1>Description about city</h1>
+                <h2>도시 이름: {cityName}</h2>
+                <p>현재 날씨 : {currentweather}</p>
+                <p>날씨에 대한 자세한 정보: {description}</p>
+                <p>기온: {temperature} 도 </p>
+            </div>
+        );
+    }
 };
-
 export default Weather;
