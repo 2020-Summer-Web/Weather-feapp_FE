63d39755f96fdbaecdb1b21964e951f10f049966
JasonKK <hyeon7427@gmail.com>
2020-07-26 23:45:52 +0900

    Update cities.js
    Update cities.js

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -3,53 +3,46 @@ import { Switch, Route, withRouter } from "react-router";
 
 import CityList from "./CityList";
 import Weather from "./Weather/index";
-
-const API_CITIES = "http://localhost:8888//weather-service/weathers/cities";
-
+// const API_CITIES = "http://localhost:8888/weather-craw.../cities";
+const API_CITIES = 'http://localhost:8888/weather-service/available-cities';
 class Cities extends React.Component {
-  state = {
-    cities: [],
-  };
+    state = {
+        cities: [],
+    };
+    componentDidMount() {
+        console.log("City component");
+        // console.log(this.state.cities);
+        const { cities } = this.state;
+        // console.log(cities);
+        const citiesData = fetch(API_CITIES)
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({
+                    cities: data,
+                });
+            });
 
-  componentDidMount() {
-    console.log("City component");
+        console.log(cities,citiesData);
+    }
+    render() {
+        const {match} = this.props;
+        const {cities} = this.state;
+        return (
+            <div>
+                <h1>Cities</h1>
+                <p>City list</p>
+                <Switch>
+                    <Route path={`${match.path}/:cityName`} component={Weather}/>
+                    <Route
+                        exact
+                        path={match.path}
+                        render={() => <CityList cities={cities}/>}
+                    />
 
-    // console.log(this.state.cities);
+                </Switch>
+            </div>
+        );
 
-    const { cities } = this.state;
-    // console.log("Cities: ", cities);
-
-    const citiesData = fetch(API_CITIES)
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({
-          cities: data,
-        });
-      });
-
-    console.log(cities, citiesData);
-  }
-
-  render() {
-    const { match } = this.props;
-    const { cities } = this.state;
-
-    return (
-      <div>
-        <h1>Cities</h1>
-        <p>City list</p>
-
-        <Switch>
-          <Route path={`${match.path}/:cityName`} component={Weather} />
-          <Route
-            exact
-            path={match.path}
-            render={() => <CityList cities={cities} />}
-          />
-        </Switch>
-      </div>
-    );
-  }
+    }
 }
-
 export default withRouter(Cities);
