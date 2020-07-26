279cb004f80fd79e4c32615db1997a044ea4d302
JasonKK <hyeon7427@gmail.com>
2020-07-26 23:46:39 +0900

    Update CityList.js
    fixed repeated cities

--- src/components/CityList.js
+++ src/components/CityList.js
@@ -3,21 +3,27 @@ import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 
 const CityList = (props) => {
-  const { cities, match } = props;
-  const { url } = match;
 
-  return (
-    <ul>
-      {cities.map((item) => {
-        // console.log(item);
-        return (
-          <li key={item}>
-            <Link to={`${url}/${item}`}>{item}</Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
+    const { cities, match } = props;
+    const { url } = match;
+
+    console.log(url);
+    const uniqueCities = cities.filter(
+        (item, index) => cities.indexOf(item) === index
+    );
+    return (
+        <ul>
+            {uniqueCities.map((item) => {
+
+                        // console.log(item);
+                        return (
+                            <li key={item}>
+                                <Link to={`${url}/${item}`}>{item}</Link>
+                            </li>
+                        );
+
+            })}
+        </ul>
+    );
 };
-
 export default withRouter(CityList);
